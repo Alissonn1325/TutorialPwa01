1bc6df9cf7e850c8624ebbc0aa99081080bce9cc
Alissonn1325 <alissonfbrito01@gmail.com]>
2024-02-27 16:30:00 -0300

    Teste 1

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.752pl8n31tg"
+    "revision": "0.uodqvat4in"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
